a8dbfb26835be557aa84df6a7ac28798f496614e
ignat99 <ignat99@spain.holding.bz>
2018-12-05 13:57:36 +0200

    No debug

--- Alphabet_soup.c
+++ Alphabet_soup.c
@@ -19,12 +19,8 @@ int iNumA=0,iNumB=0,i,j;
 TElement elTemp;
 TElement elA[N_MAX]; // { Source array, tree and result }
 TElement elB[N_MAX];
-//char cTemp; // { One symbol }
 char words[] = "Heap Array sorting by tree building q\0";
 char soup[] = "qwertyuiopasdfghjklzxcvbnm   AHeerray sorting by tree building q\0";
-
-//char words[] = "1237";
-//char soup[] = "7112233";
 
 
 int FixTree(int iFrom, int iTo, TElement *elA)
@@ -51,48 +47,21 @@ int FixTree(int iFrom, int iTo, TElement *elA)
 void main(int argc, int *arhv [])
 {
 
-  printf("\n");
-  printf(words);
-  printf("\n");
-  printf(soup);
-  printf("\n");
-
 // { Read unsorted array of words }
   for (i=0;iNumA < strlen(words);i++) {
-//    printf("%d - %c:%d, ", ++iNumA, cTemp, (int)cTemp);
     elA[++iNumA] = (int)words[i];
-//    PRINT(iNumA, elA);
-//    iNumA++;
   }
-
-
 
 // { Read unsorted array of soup }
   for (i=0;iNumB < strlen(soup);i++) {
-//    printf("%d - %c:%d, ", ++iNumB, cTemp, (int)cTemp);
     elB[++iNumB] = (int)soup[i];
-//    PRINT(iNumB, elB);
-//    iNumB++;
   }
-
 
 
 // { Build a sorting tree }
-  for ( i= (int)(iNumA / 2); i >=1; i--) { FixTree(i,iNumA,elA);
-    printf("* %d %d: ",i,iNumA);
-    PRINT(iNumA, elA);
-  }
+  for ( i= (int)(iNumA / 2); i >=1; i--) FixTree(i,iNumA,elA);
 
-  printf("\n");
-
-
-  for ( i= (int)(iNumB / 2); i >=1; i--) { FixTree(i,iNumB,elB);
-    printf("** %d %d: ",i,iNumB);
-    PRINT(iNumB, elB);
-  }
-
-  printf("\n");
-
+  for ( i= (int)(iNumB / 2); i >=1; i--) FixTree(i,iNumB,elB);
 
 // { Convert tree to array }
   for (i=iNumA; i>=2; i-- )
@@ -101,8 +70,6 @@ void main(int argc, int *arhv [])
     elA[1]=elA[i];
     elA[i]=elTemp;
     FixTree(1,i-1,elA);
-    printf("*-> %d, %d :  ", iNumA, i-1);
-    PRINT(iNumA,elA);
   }
 
   for (i=iNumB; i>=2; i-- )
@@ -111,8 +78,6 @@ void main(int argc, int *arhv [])
     elB[1]=elB[i];
     elB[i]=elTemp;
     FixTree(1,i-1,elB);
-    printf("**-> %d, %d :  ", iNumB, i-1);
-    PRINT(iNumB,elB);
   }
 
 // { Displey the result }
@@ -123,8 +88,6 @@ void main(int argc, int *arhv [])
   for(i=1,j=1;i<=iNumA;)
   {
     if (elA[i] > elB[j]) { j++;}
-    printf("i:%d, j:%d\n", i, j);
-    printf("elA[i]:%d, elB[j]:%d\n", elA[i], elB[j]);
     if (elA[i] < elB[j]) { printf("\nFalse1\n");break;}
     if (j > iNumB) { printf("\nFalse3\n");break;}
     if (elA[i]==elB[j])
@@ -132,8 +95,6 @@ void main(int argc, int *arhv [])
       if ((elA[i+1] < elB[j+1]) && (i<iNumA)) { printf("\nFalse2\n");break;}
       else if (elA[i+1] >= elB[j+1]) { i++;j++; }
       else {i++;}
-    printf("End2 i:%d, j:%d\n", i, j);
-    printf("End2 elA[i]:%d, elB[j]:%d\n", elA[i], elB[j]);
       if (i > iNumA) { printf("\nTrue2\n"); break; }
     }
   }
