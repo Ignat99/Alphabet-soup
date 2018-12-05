c769210c1a9c4ad0ccb880d11154509a09a80048
ignat99 <ignat99@spain.holding.bz>
2018-12-06 01:32:00 +0200

    v0.99

--- alphabet_soup.py
+++ alphabet_soup.py
@@ -13,6 +13,7 @@ import sys
 #SOUP = "qwertyuiopasdfghjklzxcvbnm   AHeerray sorting by tree building q"
 WORDS = "123"
 SOUP = "1122337"
+
 def FixTree(iFrom, iTo, elC):
     """Universal function for sorting tree and leaves."""
     elTop = elC[iFrom-1]
@@ -47,39 +48,37 @@ def main():
 # { Read unsorted array of words }
     iNumA = len(WORDS)
     elA = list(WORDS)
-#    print("\n",elA,"\n")
+
 # { Read unsorted array of soup }
     iNumB = len(SOUP)
     elB = list(SOUP)
-#    print("\n",elB,"\n")
+
 
 # { Build a sorting trees }
     for i in range(int(iNumA / 2), 0, -1):
         FixTree(i, iNumA, elA)
-#        print("* ", i, " ", iNumA,elA)
+
 
     for i in range(int(iNumB / 2), 0, -1):
         FixTree(i, iNumB, elB)
-#        print("** ", i, " ", iNumB,elB)
+
 
 # { Convert tree to array }
     for i in range(iNumA, 1, -1):
         elA[0], elA[i-1] = elA[i-1], elA[0]
         FixTree(1, i-1, elA)
-#        print("*-> ", iNumA, ", ", i-1,elA)
+
 
     for i in range(iNumB, 1, -1):
         elB[0], elB[i-1] = elB[i-1], elB[0]
         FixTree(1, i-1, elB)
-#        print("**-> ", iNumB, ", ", i-1,elB)
+
 
 # { Displey the result }
     print("* Sorted arrays *\n")
     print(iNumA, elA);
     print(iNumB, elB);
 
-
-#  for(i=1,j=1;i<=iNumA;)
     i = 1
     j = 1
     while i <= iNumA:
